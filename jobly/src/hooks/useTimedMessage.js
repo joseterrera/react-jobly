c5748ed861985a0696688f85b9fd2c04816aac62
Jose Terrera <joseterrera@yahoo.com>
2021-04-14 12:04:06 -0700

    adding homepage and hook

---
+++ jobly/src/hooks/useTimedMessage.js
@@ -0,0 +1,29 @@
+import { useRef, useState, useEffect } from "react";
+
+/** Custom hook for managing 'flash' messages
+ *
+ * This adds an item in state `active` which can be controlled by the component as desired. The component would typically `setActive(true)`
+*/
+
+function useTimedMessage(timeInMsec=3000) {
+  const [active, setActive] = useState(false);
+  const messageShownRef = useRef(false);
+
+  useEffect(
+    function showSavedMessage() {
+      console.debug('useTimedMessage useEffect showSavedMessage', 'active=', active);
+
+      if(active && !messageShownRef.current) {
+        messageShownRef.current = true;
+        setTimeout(function removeMessage() {
+          setActive(false);
+          messageShownRef.current = false;
+        }, timeInMsec);
+      }
+    },
+    [active, timeInMsec]
+  );
+  return [active, setActive]
+}
+
+export default useTimedMessage;
